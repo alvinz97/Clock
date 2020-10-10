8926478741a2e469addb160c46ada68c63744a8f
alvinz97 <it18211306@my.sliit.lk>
2020-10-10 22:37:33 +0530

    Project complete

--- js/app.js
+++ js/app.js
@@ -1,20 +1,51 @@
-function initialize() {
-    const deg = 6;
-    const hr = document.querySelector('#hr');
-    const min = document.querySelector('#min');
-    const sec = document.querySelector('#sec');
+// function initialize() {
+//     const deg = 6;
+//     const hr = document.querySelector('#hr');
+//     const min = document.querySelector('#min');
+//     const sec = document.querySelector('#sec');
 
-    setInterval( () => {
-        let day = new Date();
-        let hh = day.getHours() * 30;
-        let mm = day.getMinutes() * deg;
-        let ss = day.getSeconds() * deg;
+//     setInterval( () => {
+//         let day = new Date();
+//         let hh = day.getHours() * 30;
+//         let mm = day.getMinutes() * deg;
+//         let ss = day.getSeconds() * deg;
 
-        hr.style.transform = `rotateZ(${hh + mm / 12} deg)`;
-        min.style.transform = `rotateZ(${mm} deg)`;
-        sec.style.transform = `rotateZ(${ss} deg)`;
-    });
+//         hr.style.transform = `rotateZ(${hh + mm / 12} deg)`;
+//         min.style.transform = `rotateZ(${mm} deg)`;
+//         sec.style.transform = `rotateZ(${ss} deg)`;
+//     });
+// }
+
+
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector('[date-hour-hand]');
+const minuteHand = document.querySelector('[date-minute-hand]');
+const secondHand = document.querySelector('[date-second-hand]');
+
+function setClock () {
+    const currentDate = new Date();
+    const secondRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondRatio + currentDate.getMinutes()) / 60;
+
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+    setRotation(secondHand, secondRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
 }
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+
+
+setClock();
+
+
+
+
 
 function showTime(){
 	var date = new Date();
